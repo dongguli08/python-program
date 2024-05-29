c1fc43c08b749b894c2832c7c9d61d4891d7d3f9
hadonggun <hugh3232@naver.com>
2024-05-29 21:58:52 +0900

    .

--- 하동건.py
+++ 하동건.py
@@ -1,14 +1,19 @@
-from itertools import combinations
+# from itertools import combinations
 
-a,b=map(int,input("숫자의 갯수와 합 입력:").split())
-c=list(map(int,input("숫자%d개 입력:" %a).split()))
-biggest_sum=0
-
-
-for i in combinations(c,3):
-        list_sum=sum(i)
-        if biggest_sum<list_sum<=b:
-         biggest_sum=list_sum
-print(biggest_sum)
+# a,b=map(int,input("숫자의 갯수와 합 입력:").split())
+# c=list(map(int,input("숫자%d개 입력:" %a).split()))
+# biggest_sum=0
 
 
+# for i in combinations(c,3):
+#         list_sum=sum(i)
+#         if biggest_sum<list_sum<=b:
+#          biggest_sum=list_sum
+# print(biggest_sum)
+
+while(True):
+    a,b=map(int,input().split())
+    if input == 0:
+        break
+    else:
+        print(a+b)
